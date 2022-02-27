850ef72cbca61e88003a0afdd549dde2cb6d0a3f
alfajryuneda <alfajryuneda@gmail.com>
2022-02-27 13:37:33 +0700

    add final variable in dart

--- 02-variable.dart
+++ 02-variable.dart
@@ -16,16 +16,15 @@ void main() {
 
   // --------------------------- DEFAULT VALUE ---------------------------
 
-  // Berlaku null safety atau boleh null, 
+  // Berlaku null safety atau boleh null,
   // Jika berlaku null safety maka variabelnya harus diinisialisasi sebelum digunakan
   int lineCount;
-  if (condition==true) {
+  if (condition == true) {
     lineCount = 5;
   } else {
     lineCount = 0;
   }
   print(lineCount);
-
 
   // --------------------------- LATE VARIABLES ---------------------------
   // variable dengan kata "late" bisa diinisialisasi di kemudiannya
@@ -34,4 +33,11 @@ void main() {
   description = 'Feijoada!';
   print(description);
 
+  // --------------------------- FINAL AND CONST ---------------------------
+  // Gunakan final atau const apabila kita tidak akan mengubah nilai dari sebuah variabel
+  // mengubah nilai age akan memunculkan error karena penggunaan "final"
+  
+  final age = 20;
+  // age = 22;
+  print(age);
 }
