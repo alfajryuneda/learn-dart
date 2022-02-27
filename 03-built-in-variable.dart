b86ca9fcc313c29722c310d25029bd336d0606d5
alfajryuneda <alfajryuneda@gmail.com>
2022-02-27 20:27:29 +0700

    add new files

---
+++ 03-built-in-variable.dart
@@ -0,0 +1,38 @@
+void main() {
+  //----------------------------- NUMBER -----------------------------
+  var x = 100; //int
+  var y = 2.5; //double
+  int a = 10; //int
+  double b = 20.2; //double
+  num c = 70; //int or double
+  c += 4.9;
+  print('x memiliki tipe data : ${x.runtimeType}');
+  print('y memiliki tipe data : ${y.runtimeType}');
+  print('a memiliki tipe data : ${a.runtimeType}');
+  print('b memiliki tipe data : ${b.runtimeType}');
+  print('c memiliki tipe data : ${c.runtimeType}');
+
+  // string to int
+  var first = '1';
+  var firstResult = int.parse(first);
+  print('tipe data first : ${first.runtimeType}');
+  print('tipe data firstResult : ${firstResult.runtimeType}');
+
+  // string to double
+  var second = '2.0';
+  var secondResult = double.parse(second);
+  print('tipe data second : ${second.runtimeType}');
+  print('tipe data secondResult : ${secondResult.runtimeType}');
+
+  // int to string
+  var firstNum = 100;
+  var firstString = firstNum.toString();
+  print('tipe data firstNum : ${firstNum.runtimeType}');
+  print('tipe data firstString : ${firstString.runtimeType}');
+
+
+
+  //----------------------------- STRING -----------------------------
+  
+
+}
