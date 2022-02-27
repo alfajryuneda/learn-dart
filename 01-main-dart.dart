b0449cdfdc5f25c777940704513cf60509b3ab85
alfajryuneda <alfajryuneda@gmail.com>
2022-02-27 07:31:46 +0700

    Add comment

--- 01-main-dart.dart
+++ 01-main-dart.dart
@@ -1,8 +1,19 @@
+// membuat sebuah fungsi
 void printName(String myName) {
   print('My name is ${myName}');
 }
 
+// fungsi main adalah fungsi yang pertama kali dijalankan
 void main() {
+
+  // Deklarasi dan inisialisasi variabel
   var myFullName = "Achmad Yuneda Alfajr";
+
+  // Pemanggilan fungsi printName()
   printName(myFullName);
 }
+
+/*
+Di dalam fungsi main, terdapat fungsi printName()
+fungsi printName akan menampilkan string yang dimasukkan
+*/
