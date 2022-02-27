3e30d05eafa73d65bb4f81928a78e7fec16ce718
alfajryuneda <alfajryuneda@gmail.com>
2022-02-27 07:42:59 +0700

    add new variable

--- 01-main-dart.dart
+++ 01-main-dart.dart
@@ -1,16 +1,17 @@
 // membuat sebuah fungsi
-void printName(String myName) {
+void printName(String myName, int semester) {
   print('My name is ${myName}');
+  print('Im currently at semester ${semester}');
 }
 
 // fungsi main adalah fungsi yang pertama kali dijalankan
 void main() {
-
   // Deklarasi dan inisialisasi variabel
   var myFullName = "Achmad Yuneda Alfajr";
+  var semester = 6;
 
-  // Pemanggilan fungsi printName()
-  printName(myFullName);
+  // Pemanggilan fungsi printName
+  printName(myFullName, semester);
 }
 
 /*
